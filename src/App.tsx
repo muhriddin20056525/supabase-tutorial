d3214ef9f9e575e446e2fc065e07fb1dbc15461e
muhriddin20056525 <muhriddindavlatov89@gmail.com>
2025-05-08 09:45:26 +0500

    supabase auth with email and password

--- src/App.tsx
+++ src/App.tsx
@@ -1,142 +1,50 @@
-import { FormEvent, useEffect, useState } from "react";
-import { supabase } from "./supabase-client";
+import { useEffect, useState } from "react";
 
-interface Task {
-  id: number;
-  title: string;
-  description: string;
-  created_at: string;
-}
+import TaskManager from "./components/TaskManager";
+import { supabase } from "./supabase-client";
+import { Auth } from "./components/Auth";
 
 function App() {
-  const [newTask, setNewTask] = useState({ title: "", description: "" });
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newDescription, setNewDescription] = useState("");
+  const [session, setSession] = useState<any>(null);
 
-  // supabasedagi malumotlarni oladigan funksiya
-  const fetchTasks = async () => {
-    const { error, data } = await supabase
-      .from("tasks")
-      .select("*")
-      .order("created_at", { ascending: true });
+  const fetchSession = async () => {
+    // Supabase orqali joriy (hozirgi) sessiyani asinxron tarzda olish
+    const currentSession = await supabase.auth.getSession();
 
-    if (error) {
-      console.log("Error reading task ", error.message);
-      return;
-    }
-
-    setTasks(data);
-  };
-
-  // supabasega malumot qo'shadigan funksiya
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // supabasedagi jadvalga malumot qo'shish
-    const { error } = await supabase.from("tasks").insert(newTask).single();
-
-    if (error) {
-      console.log("Error adding task ", error.message);
-    }
-
-    setNewTask({ title: "", description: "" });
-  };
-
-  // supabasedan malumotlarni o'chiradigan funksiya
-  const deleteTask = async (id: number) => {
-    const { error } = await supabase.from("tasks").delete().eq("id", id);
-
-    if (error) {
-      console.log("Error adding task ", error.message);
-      return;
-    }
-  };
-
-  // supabasedagi malumotni yangilash
-  const updateTask = async (id: number) => {
-    const { error } = await supabase
-      .from("tasks")
-      .update({ description: newDescription })
-      .eq("id", id);
-
-    if (error) {
-      console.log("Error updating task ", error.message);
-      return;
-    }
+    // Olingan sessiyani React holatiga (state) saqlash
+    setSession(currentSession.data.session);
   };
 
   useEffect(() => {
-    fetchTasks();
+    fetchSession();
+    // Supabase autentifikatsiya holati o'zgarganda (login yoki logout bo'lsa) ishlaydigan listener qo'shilyapti
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        // Har safar foydalanuvchi holati o'zgarganda (masalan: login bo'lsa) session holatini yangilaymiz
+        setSession(session);
+      }
+    );
+
+    // Komponent unmount (yo'qolayotgan) bo'lganda listenerni tozalaymiz (memory leak bo'lmasligi uchun)
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
   }, []);
 
-  console.log(tasks);
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
 
   return (
-    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "1rem" }}>
-      <h2>Task Manager CRUD</h2>
-
-      {/* Form to add a new task */}
-      <form style={{ marginBottom: "1rem" }} onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Task Title"
-          onChange={(e) =>
-            setNewTask((prev) => ({ ...prev, title: e.target.value }))
-          }
-          value={newTask.title}
-          style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
-        />
-        <textarea
-          placeholder="Task Description"
-          style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
-          onChange={(e) =>
-            setNewTask((prev) => ({ ...prev, description: e.target.value }))
-          }
-          value={newTask.description}
-        />
-        <button type="submit" style={{ padding: "0.5rem 1rem" }}>
-          Add Task
-        </button>
-      </form>
-
-      {/* List of Tasks */}
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        {tasks.map((task) => (
-          <li
-            style={{
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              padding: "1rem",
-              marginBottom: "0.5rem",
-            }}
-            key={task.id}
-          >
-            <div>
-              <h3>{task.title}</h3>
-              <p>{task.description}</p>
-              <div>
-                <textarea
-                  value={newDescription}
-                  onChange={(e) => setNewDescription(e.target.value)}
-                  placeholder="Updated Description"
-                />
-                <button
-                  onClick={() => updateTask(task.id)}
-                  style={{ padding: "0.5rem 1rem", marginRight: "0.5rem" }}
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => deleteTask(task.id)}
-                  style={{ padding: "0.5rem 1rem" }}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div>
+      {session ? (
+        <>
+          <button onClick={logout}>Log out</button>
+          <TaskManager session={session} />
+        </>
+      ) : (
+        <Auth />
+      )}
     </div>
   );
 }
